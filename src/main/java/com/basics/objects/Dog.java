a0d8f39d299bc331890b24707d9569a3377b84b9
Matheus Phelipe <matheusphalves@gmail.com>
2023-09-12 22:13:00 -0300

    feat: add classes and objects section

--- src/main/java/com/basics/objects/Dog.java
+++ src/main/java/com/basics/objects/Dog.java
@@ -1,4 +1,24 @@
 package com.basics.objects;
 
+import java.util.Objects;
+
 public class Dog {
+
+    private String breed;
+    private int age;
+    private String color;
+
+    public Dog(String breed, int age, String color){
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return age == dog.age && Objects.equals(breed, dog.breed) && Objects.equals(color, dog.color);
+    }
 }
--- src/main/java/com/basics/objects/Main.java
+++ src/main/java/com/basics/objects/Main.java
@@ -1,2 +1,17 @@
-package com.basics.objects;public class Main {
+package com.basics.objects;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dog caramelo1 = new Dog("Não Definida", 2, "Caramelo");
+        Dog caramelo2 = new Dog("Não Definida", 2, "Caramelo");
+
+        Dog caramelo3 = caramelo1;
+
+        System.out.println(caramelo1);
+        System.out.println(caramelo2);
+        System.out.println(caramelo3);
+
+        System.out.println(caramelo1.equals(caramelo2));
+    }
 }
