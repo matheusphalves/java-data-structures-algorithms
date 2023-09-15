e35e1ae405d38b0294633ba6df63492128970e6d
Matheus Phelipe <matheusphalves@gmail.com>
2023-09-14 21:06:07 -0300

    feat: add node definition

---
+++ src/main/java/com/datastructures/collection/Node.java
@@ -0,0 +1,27 @@
+package com.datastructures.collection;
+
+public class Node <T> {
+    private T info;
+
+    private Node<T> next;
+
+    public Node(T info) {
+        this.info = info;
+    }
+
+    public T getInfo() {
+        return info;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
