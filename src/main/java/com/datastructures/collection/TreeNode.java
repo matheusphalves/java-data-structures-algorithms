b875b9d661903e1afa0986cd3c0c57a2f4f1b44f
Matheus Phelipe <matheusphalves@gmail.com>
2023-11-02 22:58:57 -0300

    feat: add Tree definitions

---
+++ src/main/java/com/datastructures/collection/TreeNode.java
@@ -0,0 +1,37 @@
+package com.datastructures.collection;
+
+public class TreeNode <T extends Comparable<T>> {
+
+    private T info;
+
+    private TreeNode<T> leftNode;
+    private TreeNode<T> rightNode;
+
+    public TreeNode(T info) {
+        this.info = info;
+    }
+
+    public T getInfo() {
+        return info;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+
+    public TreeNode<T> getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(TreeNode<T> leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public TreeNode<T> getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(TreeNode<T> rightNode) {
+        this.rightNode = rightNode;
+    }
+}
---
+++ src/main/java/com/datastructures/collection/api/Tree.java
@@ -0,0 +1,23 @@
+package com.datastructures.collection.api;
+
+import com.datastructures.collection.TreeNode;
+
+public interface Tree  <T extends Comparable<T>> {
+
+    void add(T element);
+
+    boolean isEmpty();
+
+    int getTreeHeight();
+
+    int nodeDepth(T element);
+
+    TreeNode<T> search(T element);
+
+    List<T> preOrder();
+
+    List<T> inOrder();
+
+    List<T> postOrder();
+
+}
